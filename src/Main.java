a4e42758382f1ed94cc51a23a99137ddf4d5108b
debrunei000 <debrunei000@warren.k12.in.us>
2025-08-22 13:26:57 -0400

    r5

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int a = input.nextInt();
+        System.out.println("Please enter a second number: ");
+        int b = input.nextInt();
 
-
+        System.out.println("Max of a and b: " + findMaximum(a, b));
         System.out.println("Max of 15 and 23: " + findMaximum(15, 23));
         System.out.println("Max of 100 and 50: " + findMaximum(100, 50));
         System.out.println("Max of -5 and -10: " + findMaximum(-5, -10));
