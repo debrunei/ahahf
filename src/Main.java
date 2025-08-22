99bc3024af578d1c24a28ceb5fa98381c784744d
debrunei000 <debrunei000@warren.k12.in.us>
2025-08-22 12:57:19 -0400

    r

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Max of 15 and 23: " + findMaximum(15, 23));
+        System.out.println("Max of 100 and 50: " + findMaximum(100, 50));
+        System.out.println("Max of -5 and -10: " + findMaximum(-5, -10));
+    }
+    public static int findMaximum(int a, int b) {
+        int max = (a > b) ? a : b;
+        return max;
+    }
 }
